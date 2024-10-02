6c7c214f29d3e5e005273b28f27035a68f98f82b
dnovak_home <dnovak@student.42.fr>
2024-10-02 13:39:04 +0200

    Added group rules to push_swap header.

--- push_swap/push_swap.h
+++ push_swap/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: dnovak <dnovak@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/20 14:51:19 by dnovak            #+#    #+#             */
-/*   Updated: 2024/09/30 15:26:59 by dnovak           ###   ########.fr       */
+/*   Updated: 2024/10/02 13:38:22 by dnovak           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,13 +34,16 @@ void		free_all(t_stack *stack);
 void		error_exit(t_stack *stack);
 void		input(int argc, char **argv, t_stack *stack);
 int			choose_to_move(int start, t_stack *stack_a, t_stack *stack_b);
+void		sort(t_stack *stack_a, t_stack *stack_b);
 
 // Rules
 void		ps_swap(t_stack *stack);
 void		ps_push(t_stack *from, t_stack *to);
 void		ps_rotate(t_stack *stack);
 void		ps_rev_rotate(t_stack *stack);
-void		sort(t_stack *stack_a, t_stack *stack_b);
+void		ps_swap_all(t_stack *stack_a, t_stack *stack_b);
+void		ps_rotate_all(t_stack *stack_a, t_stack *stack_b);
+void		ps_rev_rotate_all(t_stack *stack_a, t_stack *stack_b);
 
 // Sort algorithms
 void		adv_bubble_sort(int size, t_stack *stack_a, t_stack *stack_b);
